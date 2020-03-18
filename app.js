bbc341cda462f694583f7f31ce3a843c3e9e2d4f
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-03-17 21:01:07 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 2000
+
+var app = express();
+var publicPath = path.resolve(__dirname, 'client');	//to use the css styles
+var serv = require("http").Server(app);
+app.use(express.static(publicPath));
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "client/index.html");
+});
+app.use("/client", express.static(__dirname + "/client"));
+
+serv.listen(PORT); // Local host port
+console.log("Starting Server..."); // When the server is started
+
+
+var SOCKET_LIST = {}; 
+var io = require("socket.io")(serv,{});
+io.sockets.on("connection", function(socket){
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;
+  socket.on("disconnect", function(){
+    delete SOCKET_LIST[socket.id];
+  });
+});
